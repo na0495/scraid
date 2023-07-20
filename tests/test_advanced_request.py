004d71081533abeb0edbcfc897d31bd6d2a77370
Saad Mrabet <36574492+na0495@users.noreply.github.com>
2023-07-20 15:26:05 +0000

    fix lint

--- tests/test_advanced_request.py
+++ tests/test_advanced_request.py
@@ -1,13 +1,15 @@
 import unittest
 from scraid.advanced_request import AdvancedRequest
 
-# ----------------------------------------------------- 
+# -----------------------------------------------------
+
 
 class TestAdvancedRequest(unittest.TestCase):
     def test_headers(self):
         request = AdvancedRequest('http://example.com')
         self.assertIn('User-Agent', request.headers)
         self.assertIsNotNone(request.headers['User-Agent'])
+
 
 if __name__ == '__main__':
     unittest.main()
