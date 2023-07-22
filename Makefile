7cebd83f12871fdd0410931c3b38e11851efa81a
na0495 <saadmrabet008@gmail.com>
2023-07-22 09:21:44 +0100

    fix: small fixes

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ setup:
 	python3 setup.py sdist && \
 	python3 setup.py bdist_wheel --universal && \
 	python3 -m twine upload dist/*
+
+test:
+	python3 -m unittest discover -s tests
--- scraid/advanced_request.py
+++ scraid/advanced_request.py
@@ -6,8 +6,8 @@ from fake_useragent import UserAgent
 
 class AdvancedRequest(Request):
     """
-    AdvancedRequest is a subclass of scrapy.Request that sets a random User-Agent header for each request,
-    using the fake-useragent package, to avoid being blocked by websites that detect and block spiders.
+        AdvancedRequest is a subclass of scrapy.Request that sets a random User-Agent header for each request,
+        using the fake-useragent package, to avoid being blocked by websites that detect and block spiders.
     """
 
     def __init__(self, *args, **kwargs):
